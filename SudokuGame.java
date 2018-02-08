38a79c405c8852abbd6473fe9e8736412c3c3e66
maxdudek <36005725+maxdudek@users.noreply.github.com>
2018-02-07 19:12:10 -0500

    Update SudokuGame.java

--- SudokuGame.java
+++ SudokuGame.java
@@ -12,23 +12,60 @@ public class SudokuGame /*implements SudokuInterface*/ {
     private static int currentStep = 0;
     private static int[][] game;
 
-    
-    public static void main(String[] args) throws IOException {        
+    public static void main(String[] args) throws IOException {
         new SudokuGame();
     }
-    
+
     SudokuGame() throws IOException {
         game = retrieveSudokuGameFromFile("sudokuGame.sud");
         boolean hasAGameUpdateOccured;
+        int group;
         printBoard();
 
         while (!hasPuzzleBeenSolved()) {
             hasAGameUpdateOccured = false;
             for (int row = 0; row < BOARD_SIZE; row++) {
                 for (int col = 0; col < BOARD_SIZE; col++) {
-                    if (isCellEmpty(row,col) && isOnlyOneValueAllowedInTheCurrentCell(row,col)) {
-                        updateGame(getTheUniqueAllowedValue(row,col), row, col);
-                        hasAGameUpdateOccured = true;
+                    if (isCellEmpty(row, col)) {
+                        //Print Allowed Values in Row
+                        System.out.println("---------------------------------");
+                        System.out.print("(");
+                        for (int i = 0; i < BOARD_SIZE; i++) {
+                            System.out.print(getAllowedValuesInCurrentRow(row)[i] + ",");
+                        }
+                        System.out.println(")     Allowed values in row " + (row + 1));
+
+                        //Print Allowed Values in Column
+                        System.out.print("(");
+                        for (int i = 0; i < BOARD_SIZE; i++) {
+                            System.out.print(getAllowedValuesInCurrentCol(col)[i] + ",");
+                        }
+                        System.out.println(")     Allowed values in col " + (col + 1));
+
+                        //Print Allowed Values in Group
+                        group = 3 * (row / 3) + (col / 3);
+                        System.out.print("(");
+                        for (int i = 0; i < BOARD_SIZE; i++) {
+                            System.out.print(getAllowedValuesInCurrentGroup(row, col)[i] + ",");
+                        }
+                        System.out.println(")     Allowed values in group " + (group + 1));
+
+                        //Print Allowed Values in Cell
+                        System.out.print("(");
+                        for (int i = 0; i < getAllowedValuesInTheCurrentCell(row, col).length; i++) {
+                            System.out.print(getAllowedValuesInTheCurrentCell(row, col)[i] + ",");
+                        }
+                        int spaces = 23 - (getAllowedValuesInTheCurrentCell(row, col).length * 2);
+                        System.out.printf(")");
+                        for (int i = 0; i < spaces; i++) {
+                            System.out.print(" ");
+                        }
+                        System.out.println("Allowed values in cell (" + (col + 1) + "," + (row + 1) + ")");
+
+                        if (isOnlyOneValueAllowedInTheCurrentCell(row, col)) {
+                            updateGame(getTheUniqueAllowedValue(row, col), row, col);
+                            hasAGameUpdateOccured = true;
+                        }
                     }
                 }
             }
@@ -39,7 +76,7 @@ public class SudokuGame /*implements SudokuInterface*/ {
             }
         }
     }
-    
+
     public int[][] retrieveSudokuGameFromFile(String sudokuFile) throws IOException {
         Scanner fileInput = new Scanner(new File(sudokuFile));
         String oneRow;
@@ -47,68 +84,62 @@ public class SudokuGame /*implements SudokuInterface*/ {
         for (int row = 0; row < BOARD_SIZE; row++) {
             oneRow = fileInput.nextLine();
             for (int col = 0; col < BOARD_SIZE; col++) {
-                board[row][col] = Integer.parseInt(oneRow.charAt(2*col)+"");
+                board[row][col] = Integer.parseInt(oneRow.charAt(2 * col) + "");
             }
         }
         return board;
     }
+
     public void printBoard() {
         for (int row = 0; row < BOARD_SIZE; row++) {
             for (int col = 0; col < BOARD_SIZE; col++) {
                 System.out.print(game[row][col]);
-                if (col == 2 || col == 5)
+                if (col == 2 || col == 5) {
                     System.out.print("|");
-                else 
+                } else {
                     System.out.print(" ");
+                }
             }
             System.out.println("");
-            if (row == 2 || row == 5)
+            if (row == 2 || row == 5) {
                 System.out.println("-----------------");
+            }
         }
+        System.out.println("");
     }
-    
+
     public boolean isCellEmpty(int row, int col) {
         return game[row][col] == EMPTY_CELL;
     }
-    
+
     public boolean isOnlyOneValueAllowedInTheCurrentCell(int row, int col) {
-        return getAllowedValuesInTheCurrentCell(row,col).length == 1;
+        return getAllowedValuesInTheCurrentCell(row, col).length == 1;
     }
-    
+
     public int[] getAllowedValuesInTheCurrentCell(int row, int col) {
         int[] allowedValuesInCurrentRow = getAllowedValuesInCurrentRow(row);
         int[] allowedValuesInCurrentCol = getAllowedValuesInCurrentCol(col);
-        int[] allowedValuesInCurrentGroup = getAllowedValuesInCurrentGroup(row,col);
-        
+        int[] allowedValuesInCurrentGroup = getAllowedValuesInCurrentGroup(row, col);
+
         int[] temp = new int[BOARD_SIZE];
         int index = 0;
-        
+
         for (int i = 0; i < BOARD_SIZE; i++) {
-            if (allowedValuesInCurrentRow[i] != 0 &&
-                allowedValuesInCurrentCol[i] != 0 &&
-                allowedValuesInCurrentGroup[i] != 0) {
-                    temp[index] = i+1;
-                    index++;
+            if (allowedValuesInCurrentRow[i] != 0
+                    && allowedValuesInCurrentCol[i] != 0
+                    && allowedValuesInCurrentGroup[i] != 0) {
+                temp[index] = i + 1;
+                index++;
             }
         }
-        
+
         int[] allowedValues = new int[index];
         for (int i = 0; i < index; i++) {
             allowedValues[i] = temp[i];
         }
-        
-        System.out.print("(");
-        for (int i = 0; i < allowedValues.length; i++) {
-            System.out.print(allowedValues[i] + ",");
-        }
-        int spaces = 23 - (allowedValues.length*2);
-        System.out.printf(")");
-        for (int i = 0; i < spaces; i++)
-            System.out.print(" ");
-        System.out.println("Allowed values in cell (" + (col+1) + "," + (row+1) + ")");
-        
         return allowedValues;
     }
+
     public int[] getAllowedValuesInCurrentRow(int row) {
         int[] allowedValueInRow = new int[BOARD_SIZE];
         boolean hasValueInRow;
@@ -123,16 +154,11 @@ public class SudokuGame /*implements SudokuInterface*/ {
                 allowedValueInRow[i - 1] = i;
             }
         }
-        System.out.println("---------------------------------");
-        System.out.print("(");
-        for (int k = 0; k < BOARD_SIZE; k++) {
-            System.out.print(allowedValueInRow[k] + ",");
-        }
-        System.out.println(")     Allowed values in row " + (row + 1));
+
         return allowedValueInRow;
-    
+
     }
-    
+
     public int[] getAllowedValuesInCurrentCol(int col) {
         int[] allowedValueInCol = new int[BOARD_SIZE];
         boolean hasValueInCol;
@@ -147,59 +173,53 @@ public class SudokuGame /*implements SudokuInterface*/ {
                 allowedValueInCol[i - 1] = i;
             }
         }
-        System.out.print("(");
-        for (int l = 0; l < BOARD_SIZE; l++) {
-            System.out.print(allowedValueInCol[l] + ",");
-        }
-        System.out.println(")     Allowed values in col " + (col + 1));
+
         return allowedValueInCol;
-        
+
     }
+
     public int[] getAllowedValuesInCurrentGroup(int row, int col) {
         int corner_row = (row / 3) * 3;
         int corner_col = (col / 3) * 3;
-        int group = 3*(row/3) + (col/3);
         int cellValue;
         int[] allowedValuesInCurrentGroup = new int[BOARD_SIZE];
         for (int i = 0; i < GROUP_SIZE; i++) {
             for (int j = 0; j < GROUP_SIZE; j++) {
-                cellValue = game[corner_row+i][corner_col+j];
-                if (cellValue != EMPTY_CELL)
-                    allowedValuesInCurrentGroup[cellValue-1] = cellValue;
+                cellValue = game[corner_row + i][corner_col + j];
+                if (cellValue != EMPTY_CELL) {
+                    allowedValuesInCurrentGroup[cellValue - 1] = cellValue;
+                }
             }
         }
         //Reverse the array
         for (int i = 0; i < BOARD_SIZE; i++) {
             cellValue = allowedValuesInCurrentGroup[i];
             if (cellValue == EMPTY_CELL) {
-                allowedValuesInCurrentGroup[i] = i+1;
+                allowedValuesInCurrentGroup[i] = i + 1;
             } else {
                 allowedValuesInCurrentGroup[i] = 0;
             }
         }
-        //Print
-        System.out.print("(");
-        for (int l = 0; l < BOARD_SIZE; l++) {
-            System.out.print(allowedValuesInCurrentGroup[l] + ",");
-        }
-        System.out.println(")     Allowed values in group " + (group + 1));
+
         return allowedValuesInCurrentGroup;
     }
-    
+
 //    public int[] getAllowedValuesBasedOnTheThreeRules(int[] allowedValuesInCurrentRow, 
 //            int[] allowedValuesInCurrentCol, int[] allowedValuesInCurrentGroup) {
 //        
 //    }
     public int getTheUniqueAllowedValue(int row, int col) {
-        int[] allowedValues = getAllowedValuesInTheCurrentCell(row,col);
+        int[] allowedValues = getAllowedValuesInTheCurrentCell(row, col);
         return allowedValues[0];
     }
+
     public void updateGame(int allowedValue, int row, int col) {
         game[row][col] = allowedValue;
         currentStep++;
-        System.out.println("\nStep " + currentStep + ": " + allowedValue + " is placed at row: " + (row+1) + " column: " + (col+1) + "\n");
+        System.out.println("\nStep " + currentStep + ": " + allowedValue + " is placed at row: " + (row + 1) + " column: " + (col + 1) + "\n");
         printBoard();
     }
+
     public boolean hasPuzzleBeenSolved() {
         for (int row = 0; row < BOARD_SIZE; row++) {
             for (int col = 0; col < BOARD_SIZE; col++) {
@@ -210,7 +230,7 @@ public class SudokuGame /*implements SudokuInterface*/ {
         }
         showGameSolvedMessage();
         return true;
-     }
+    }
 
     public void showGameSolvedMessage() {
         System.out.println("\nGame solved.");
